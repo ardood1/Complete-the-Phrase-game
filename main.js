26c3d2db44d8fe3975448cd4ecfe6067797689cd
ardood1 <125295008+ardood1@users.noreply.github.com>
2023-02-13 09:50:58 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// Complete the Phrase Exercise
+
+// Button 1 - Get your act together
+document.getElementById("btn1").addEventListener("click", btn1Clicked);
+
+function btn1Clicked() {
+  alert("...together");
+}
+
+// Button 2 - A dime a dozen
+document.getElementById("btn2").addEventListener("dblclick", btn2Clicked);
+
+function btn2Clicked() {
+  alert("...dozen");
+}
+
+// Button 3 - Better late than
+document.getElementById("btn3").addEventListener("click", btn3Clicked);
+
+function btn3Clicked() {
+  alert("...never");
+}
+
+// Button 4 - No pain no gain
+document.getElementById("btn4").addEventListener("click", btn4Clicked);
+
+function btn4Clicked() {
+  alert("...no gain");
+}
+
+// Button 5 - Time flies when You're having fun
+document.getElementById("btn5").addEventListener("click", btn5Clicked);
+
+function btn5Clicked() {
+  alert("...when You're having fun");
+}
